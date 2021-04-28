eb978fdba6a1d6bb5c09535524e8cd249f35a9cc
Ali Dinç <normprojects@outlook.com>
2021-04-28 13:58:18 +0100

    1.0 Simpsons Book

---
+++ Simpsons Book/Model/Simpson.swift
@@ -0,0 +1,20 @@
+//
+//  Simpson.swift
+//  Simpsons Book
+//
+//  Created by Ali Dinc on 28/04/2021.
+//
+
+import UIKit
+
+class Simpson {
+    var name: String
+    var job: String
+    var image: UIImage
+    
+    init(simpsonName: String, simpsonJob: String, simpsonImage: UIImage) {
+        self.name = simpsonName
+        self.job = simpsonJob
+        self.image = simpsonImage
+    }
+}
